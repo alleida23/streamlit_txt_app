898ed76b2d6a88f9aa9bc2c855895b83143892b6
alleida23 <124719215+alleida23@users.noreply.github.com>
2023-08-31 20:34:14 +0200

    Update streamlit_txt_to_excel_app.py
    change text order and filters

--- streamlit_txt_to_excel_app.py
+++ streamlit_txt_to_excel_app.py
@@ -66,17 +66,26 @@ if st.button("Convert"):
         initial_rows_account = df.shape[0]
         df = df[df['Account'].astype(int) > 5000]
         dropped_rows_account = initial_rows_account - df.shape[0]
-        st.write(f"- 'Account' values to drop: account number <= 5000")
-        st.write(f"Eliminated entries in 'Account': **{dropped_rows_account}**")
+        #st.write(f"- 'Account' values to drop: account number <= 5000")
+        #st.write(f"Eliminated entries in 'Account': **{dropped_rows_account}**")
         
         # Drop rows from 'Subcuenta' column with specific values
         specific_values_to_drop = ['184812', '184650', '184902', '184716', '184760', '184761']
         initial_rows_subcuenta = df.shape[0]
         df = df[~df['Subcuenta'].isin(specific_values_to_drop)]
         dropped_rows_subcuenta = initial_rows_subcuenta - df.shape[0]
+        #st.write(f"- 'Subcuenta' values to drop: {', '.join(map(str, specific_values_to_drop))}")
+        #st.write(f"Eliminated entries in 'Subcuenta' values: **{dropped_rows_subcuenta}**")
+
+        # Print Filters
+        st.write(f"Filters:")
+        st.write(f"- 'Account' values to drop: account number <= 5000")
         st.write(f"- 'Subcuenta' values to drop: {', '.join(map(str, specific_values_to_drop))}")
-        st.write(f"Eliminated entries in 'Subcuenta' values: **{dropped_rows_subcuenta}**")
-        
+
+        # Print Eliminated Entries
+        st.write(f"Eliminated entries in 'Account': **{dropped_rows_account}**")
+        st.write(f"Eliminated entries in 'Subcuenta': **{dropped_rows_subcuenta}**")
+
         # Final df length after dropping rows
         final_length = len(df)
         st.write(f" ")
