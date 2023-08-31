430ceb0fcd3be91803de1ba7354202ab27689ca2
alleida23 <124719215+alleida23@users.noreply.github.com>
2023-08-31 20:15:55 +0200

    Update streamlit_txt_to_excel_app.py
    subcuenta values different line

--- streamlit_txt_to_excel_app.py
+++ streamlit_txt_to_excel_app.py
@@ -66,19 +66,20 @@ if st.button("Convert"):
         initial_rows_account = df.shape[0]
         df = df[df['Account'].astype(int) > 5000]
         dropped_rows_account = initial_rows_account - df.shape[0]
-        st.write(f"    - Eliminated accounting entries where 'Account' number <= 5000: **{dropped_rows_account}**")
+        st.write(f"    - Eliminated entries where 'Account' number <= 5000: **{dropped_rows_account}**")
 
         # Drop rows from 'Subcuenta' column with specific values
         specific_values_to_drop = ['184812', '184650', '184902', '184716', '184760', '184761']
         initial_rows_subcuenta = df.shape[0]
         df = df[~df['Subcuenta'].isin(specific_values_to_drop)]
         dropped_rows_subcuenta = initial_rows_subcuenta - df.shape[0]
-        st.write(f"    - Eliminated accounting entries for 'Subcuenta' values ({', '.join(map(str, specific_values_to_drop))}): **{dropped_rows_subcuenta}**")
+        st.write(f"    - Eliminated entries for 'Subcuenta' values*: **{dropped_rows_subcuenta}**")
+        st.write(f"    * 'Subcuenta' values to drop: {', '.join(map(str, specific_values_to_drop))}")
 
 
         # Final df length after dropping rows
         final_length = len(df)
-        st.write(f"Number of accounting entries after dropping rows: **{final_length}**")
+        st.write(f"Final number of accounting entries: **{final_length}**")
 
         # Convert 'Account', 'Compañía', 'Num Centro', 'Cuenta', and 'Subcuenta' columns to string/object
         df['Account'] = df['Account'].astype(str)
